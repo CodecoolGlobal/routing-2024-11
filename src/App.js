71700fcca5ea81a2c479d2fc8868aa52c62869a0
Gábor Hajdu <gabor.hajdu@codecool.com>
2024-11-13 10:30:34 +0100

    routing vége

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Link, Outlet, RouterProvider, useParams } from 'react-router-dom';
 import './App.css';
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home /> 
-  },
-  {
-    path: '/about',
-    element: <About />
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/user/:username',
+        element: <User />
+      }
+    ]
   }
 ]);
 
+function User() {
+  const { username } = useParams();
+  return (
+    <>
+      <h1>User: {username}</h1>
+    </>
+  )
+}
+
 function Home() {
-  return <h1>Home Page</h1>
+  return (
+    <>
+      <h1>Home Page</h1>
+    </>
+  )
 }
 
 function About() {
-  return <h1>About Page</h1>
+  return (
+    <>
+      <h1>About Page</h1>
+    </>
+  )
+}
+
+function Layout() {
+  return (
+    <div className="Layout">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/user/adam">Ádám</Link>
+        <Link to="/user/lilla">Lilla</Link>
+      </nav>
+      <div className="router-outlet">
+        <Outlet />
+      </div>
+    </div>
+  )
 }
 
 function App() {
